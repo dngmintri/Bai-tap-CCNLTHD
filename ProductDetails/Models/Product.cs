23b85d2948897e1567a89c66a6159b005b5131b4
kaiwu24 <khoavu1831@gmail.com>
2025-10-30 13:49:01 +0700

    feat: thêm bài tập Blazor

--- ProductDetails/Models/Product.cs
+++ ProductDetails/Models/Product.cs
@@ -1,10 +1,9 @@
-namespace ProductDetails.Models
+namespace ProductDetails.Models;
+
+public class Product
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = "";
-        public decimal Price { get; set; }
-        public string? Description { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
 }
